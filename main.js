8cafd93841579d037f389ca22eafb287c3ea8657
ItsZeus1 <106382063+ItsZeus1@users.noreply.github.com>
2022-09-05 15:32:45 -0400

    .

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 function close() {document.getElementById('close').onclick.style.display = none;}
+
+function closeSideBar(){
+
+    document.getElementsByClassName('sidebar').style.display = none;
+}
